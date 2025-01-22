ddbbbb5f711549471f3c7d70e703b5b9eb2d69b6
Apaaja04 <Angelkarin04@gmail.com>
2025-01-23 00:58:49 +0900

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
         <h1>Pesan Sekarang<br>Restoran Ruang Saji</h1>
         <p>Lebih Mudah, dan Cepat</p>
         <!-- Tombol yang mengarah ke halaman login -->
-        <a href="{{ route('login.tampil') }}" class="btn btn-light">Reservasi</a>
+        <a href="{{ route('login') }}" class="btn btn-light">Reservasi</a>
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,30 @@
 <?php
 
-use App\Http\Controllers\AuthenticatedSessionController;;
+use App\Http\Controllers\AuthenticatedSessionController;
 use App\Http\Controllers\ReservasiController;
 use App\Http\Controllers\MejaController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\PemesananController;
 use App\Http\Controllers\TransaksiController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 // Home
-Route::get('/home', function () {
-    return view('home');
-});
+Route::get('/', [HomeController ::class, 'index']);
 
-Route::get('/registrasi', [AuthenticatedSessionController ::class, 'tampilRegistrasi'])->name('registrasi.tampil');
-Route::post('/registrasi/submit', [AuthenticatedSessionController ::class, 'submitRegistrasi'])->name('registrasi.submit');
 
-Route::get('/login', [AuthenticatedSessionController ::class, 'tampillogin'])->name('login.tampil');
-Route::post('/login/submit', [AuthenticatedSessionController ::class, 'submitlogin'])->name('login.submit');
+//Route::get('/registrasi', [AuthenticatedSessionController ::class, 'tampilRegistrasi'])->name('registrasi.tampil');
+//Route::post('/registrasi/submit', [AuthenticatedSessionController ::class, 'submitRegistrasi'])->name('registrasi.submit');
+
+// Route::get('/login', [AuthenticatedSessionController ::class, 'tampillogin'])->name('login.tampil');
+//Route::post('/login/submit', [AuthenticatedSessionController ::class, 'submitlogin'])->name('login.submit');
 
 // Rute Logout untuk Authenticated User
-Route::middleware('auth')->group(function () {
+//Route::middleware('auth')->group(function () {
     // Proses logout
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
-});
+  //  Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/reservasi', [ReservasiController::class, 'tampilForm'])->name('reservasi.form');
@@ -44,3 +40,13 @@ Route::post('/reservasi', [MejaController::class, 'store'])->name('meja.store');
 Route::resource('menus', MenuController::class);
 Route::resource('pemesanans', PemesananController::class);
 Route::resource('transaksis', TransaksiController::class);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
